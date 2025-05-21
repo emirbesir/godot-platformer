bfe5b0f7365efa9668d8102e4fa06660a4922b40
Emir <emirbesirx@gmail.com>
2025-05-21 18:13:45 +0300

    feat(game_manager): implement score tracking and update score label on coin collection

---
+++ scripts/game_manager.gd
@@ -0,0 +1,9 @@
+extends Node
+
+var score = 0
+
+@onready var score_label: Label = $ScoreLabel
+
+func add_point(point : int) -> void:
+	score += point
+	score_label.text = "You collected " + str(score) + " coins!"
